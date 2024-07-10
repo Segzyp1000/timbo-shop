363724ebc75957a4ce50de04ff5f1c84369f3d8e
segun olowoyeye <segunolowoyeye@gmail.com>
2024-07-10 10:46:22 +0100

    added

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -3,10 +3,7 @@ import pic from "../asset/editted 1.png";
 
 const Preview = () => {
   return (
-    <div
-      className="bg-navColor text-navText mt-10 py-12 lg:py-24"
-      id="new-item"
-    >
+    <div className="bg-navColor text-navText mt-10 py-12 lg:py-24">
       <div className="container mx-auto px-4 lg:px-8 flex flex-col lg:flex-row items-center">
         <div className="text-center lg:text-left mb-8 lg:mb-0">
           <h1 className="text-3xl lg:text-[48px] font-normal tracking-widest mb-2">
@@ -17,7 +14,7 @@ const Preview = () => {
           </span>
           <div className="mt-6">
             <a
-              href="#product"
+              href="#new-item"
               className="bg-navText text-navColor font-semibold p-2 rounded-lg"
             >
               Shop Now
