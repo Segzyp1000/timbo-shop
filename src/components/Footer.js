5a2cfcc0a7836e2c5078e4389da171b600995ad9
segun olowoyeye <segunolowoyeye@gmail.com>
2024-08-23 18:17:42 +0100

    added new footer bg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { FaFacebook, FaWhatsapp, FaTwitterSquare, FaInstagramSquare } from "react-icons/fa";
 const Footer = () => {
   return (
-    <footer className='bg-navColor text-navText py-6'>
+    <footer className='bg-gray-800 text-white py-6'>
       <div className='container mx-auto px-4 flex flex-col md:flex-row justify-between items-center'>
         <p className='mb-4 md:mb-0'>
           &copy; 2024 All rights reserved
